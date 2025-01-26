6b84d64dc6ee024c71ab9f27182534bca02c22ed
Ray Bunnage <bunnage.ray@gmail.com>
2025-01-26 03:11:18 -0800

    more cleanup to make it work in vercel

--- packages/auth-service/tsup.config.ts
+++ packages/auth-service/tsup.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
+  format: ['cjs', 'esm'],
   dts: true,
-  splitting: false,
-  sourcemap: true,
   clean: true,
-  external: ['react', 'react-dom']
+  external: ['react', 'react-router-dom']
 }); 
