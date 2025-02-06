6389232597dee59e0f3f5c8d7786949b1b873f25
Ray Bunnage <bunnage.ray@gmail.com>
2025-02-06 08:29:53 -0800

    try again to see what deployment is doing

--- apps/dhg-test/vite.config.ts
+++ apps/dhg-test/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { type UserConfig, defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
+// Using type assertion to ensure correct plugin type
+const config: UserConfig = {
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+}
+
+export default defineConfig(config)
