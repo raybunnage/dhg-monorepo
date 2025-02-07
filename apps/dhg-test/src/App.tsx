f17d50b9c104bfce9d83607d552e232373651811
Ray Bunnage <bunnage.ray@gmail.com>
2025-02-06 18:30:04 -0800

    celebrate love

--- apps/dhg-test/src/App.tsx
+++ apps/dhg-test/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR and celebrate!
+          Edit <code>src/App.tsx</code> and save to test HMR and celebrate Love!
         </p>
       </div>
       <p className="read-the-docs">
